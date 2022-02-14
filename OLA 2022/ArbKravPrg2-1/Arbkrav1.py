fba62d6c7c831c1ca14bcec4e2e9ecd76c1564f2
thaffy <56458511+thaffy@users.noreply.github.com>
2022-02-14 13:53:20 +0100

    mer

--- OLA 2022/ArbKravPrg2-1/Arbkrav1.py
+++ OLA 2022/ArbKravPrg2-1/Arbkrav1.py
@@ -2,10 +2,11 @@ import os
 
 vareliste = []
 
+
 varefil = open('Varer.txt','r')
 varenr = varefil.readline()
 
-while varnr != '':
+while varenr != '':
     betegnelse = varefil.readline()
     pris = varefil.readline()
     kategori = varefil.readline()
@@ -23,6 +24,23 @@ while varnr != '':
 
 varefil.close()
 print(vareliste)
+
+listelengde = len(vareliste)
+index = 2
+
+for i in range(listelengde):
+
+        # Last i elements are already in place
+        for j in range(0, listelengde-i-1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if int(vareliste[j][index]) > int(vareliste[j+1][index]) :
+                vareliste[j], vareliste[j+1] = vareliste[j+1], vareliste[j]
+print()
+print(vareliste)
+
 
 
 
