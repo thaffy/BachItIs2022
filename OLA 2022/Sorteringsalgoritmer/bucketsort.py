94a4723f939a0626417b3c4096a428f41b80eacb
thaffy <56458511+thaffy@users.noreply.github.com>
2022-02-15 00:53:55 +0100

    bucketsort,start

--- OLA 2022/Sorteringsalgoritmer/bucketsort.py
+++ OLA 2022/Sorteringsalgoritmer/bucketsort.py
@@ -1,1 +1,10 @@
-#tbd
+def insertionsort(liste):
+    i = 1
+    while i < listelengde:
+        j = i
+        while j > 0 and liste[j-1] > liste[j]:
+            k = liste[j]
+            liste[j] = liste[j-1]
+            liste[j-1] = k
+            j -= 1
+        i += 1
