8d75e8b04f18d2a81c4797ccc6e683fb0d9ef5a0
thaffy <56458511+thaffy@users.noreply.github.com>
2022-02-16 16:27:28 +0100

    Litt forskjellig

--- OLA 2022/Sorteringsalgoritmer/1_Bubblesort/1_bubblesort.py
+++ OLA 2022/Sorteringsalgoritmer/1_Bubblesort/1_bubblesort.py
@@ -1,5 +1,8 @@
 ###############################################
 # Sortering av en vanlig liste med tall.
+from tkinter import N
+
+
 liste = [1,5,4,7,12,78,67,54,3,2,55,43,32,13,14,1,90,87]
 print(liste)
 
@@ -19,6 +22,19 @@ print(liste)
 print()
 
 
+#        [0]  [1]  [2]  [3]   [4] ---->
+liste = ['mann','dame','bil','tog','fly','fugl','vann','sjø','sko']
+print(liste[4])
+
+# #        [n]    [n+1]     [n+2]    [n+3] ] ---->
+# liste = [mann,damejakke,bilmotor,togstasjon,fly,fugl,vann,sjø,sko]
+
+# #           [0][0]     [0][1]      [1][0]      [1][1]
+# liste = [[hoppeslott,togstasjon],[helikopter,ryggsekk]]
+
+# #           [n][0]    [n][1]       [n+1][0]   [n+1][1]
+# liste = [[hoppeslott,togstasjon],[helikopter,ryggsekk]]
+# print(liste[n+1][1])
 
 
 
