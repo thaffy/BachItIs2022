a4929f90bd4ffde6b71f74eef5ffdfb056454243
thaffy <56458511+thaffy@users.noreply.github.com>
2022-02-24 12:12:48 +0100

    chordsuggest

--- Prosjekter/ChordSuggester/chordsuggester.py
+++ Prosjekter/ChordSuggester/chordsuggester.py
@@ -1,4 +1,13 @@
+# Todo:
+# -. Kartlegge/lagre alle tonearter
+# -. Kartlegge/lagre alle/mange akkordvarianter/toneart-varianter
+# -. Skrive kode for å genere tilfeldig neste akkord/akkordrekke (uten å ta til høyde for musikkteori)
+#       -. Implementere konsepter fra musikkteori
+#       -. Implementere "Følelse"-basert akkord generering (kirke-tonarter?)
+# -. Kartlegge alle noter i hver akkord
+
+
 key_C = ['C','Dm','Em','F','G','Am','Bdim']
 key_D = ['D','Em','Fm','G','A','Bm','Cdim']
-key_E = []
+key_E = ['E']
 key_Cm = ['Cm','Ddim','Eb','Fm','Gm','Ab','Bb']
