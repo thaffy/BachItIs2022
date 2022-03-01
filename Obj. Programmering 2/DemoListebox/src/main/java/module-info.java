7a798e698afffa0ffabc4f5f7d0ba1426250fb33
thaffy <56458511+thaffy@users.noreply.github.com>
2022-03-01 12:55:47 +0100

    init demolistebox

---
+++ Obj. Programmering 2/DemoListebox/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.demolistebox {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.demolistebox to javafx.fxml;
+    exports com.demolistebox;
+}
