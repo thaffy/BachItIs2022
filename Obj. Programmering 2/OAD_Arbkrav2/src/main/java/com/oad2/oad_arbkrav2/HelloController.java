c59815dacec589580eea573fdc97980f38d5cae0
thaffy <56458511+thaffy@users.noreply.github.com>
2022-04-04 10:06:39 +0200

    init oad

---
+++ Obj. Programmering 2/OAD_Arbkrav2/src/main/java/com/oad2/oad_arbkrav2/HelloController.java
@@ -0,0 +1,14 @@
+package com.oad2.oad_arbkrav2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
