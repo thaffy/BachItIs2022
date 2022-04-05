3689592e5754a66f5f43271de25b96982280f208
thaffy <56458511+thaffy@users.noreply.github.com>
2022-04-05 02:55:11 +0200

    init

--- Obj. Programmering 2/OBJ_Arbkrav_2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.objprg2.obj_arbkrav_2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.objprg2.obj_arbkrav_2 to javafx.fxml;
-    exports com.objprg2.obj_arbkrav_2;
-}
