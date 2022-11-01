cfb7c93257b15d0089643dacc3778bf2df55148c
Markus Hamre <markushamre@hotmail.com>
2022-11-01 11:11:16 +0100

    forelesninger

---
+++ AI3000/test.py
@@ -0,0 +1,28 @@
+
+
+def slettStudent():
+    studnr = input("Skriv inn studentnummer: ")
+
+
+
+
+
+
+
+def endreStudent():
+    print("Endre")
+
+def visStudent():
+    print("Vis")
+
+
+
+def main():
+    valg = input("Velg: ")
+    if valg == "1":
+        slettStudent()
+    else:
+        if valg == "2":
+            endreStudent()
+
+
